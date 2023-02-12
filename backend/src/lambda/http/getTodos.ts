e47b90fe6b5a2a53a131b79be00ae0fa94b05e58
Abdulmumin Eneye Abdulkarim <abdulmumineabdulkarim@gmail.com>
2023-02-12 20:34:43 +0100

    fixed a bug

--- backend/src/lambda/http/getTodos.ts
+++ backend/src/lambda/http/getTodos.ts
@@ -23,7 +23,9 @@ export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEven
         'Access-Control-Allow-Origin': '*',
         'Access-Control-Allow-Credentials': true
       },
-      body: JSON.stringify({ todoList })
+      body: JSON.stringify({ 
+        items: todoList
+       })
     };
   } catch (error) {
     logger.error(`Error: ${error.message}`);
